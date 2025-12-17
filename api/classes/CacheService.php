0fd878e7ed51e653d5f8b3c8a9ce96e0f105a618
Bob Coret <bob.coret+github@gmail.com>
2025-12-17 02:18:34 +0100

    changed env variable

--- api/classes/CacheService.php
+++ api/classes/CacheService.php
@@ -7,12 +7,12 @@ class CacheService {
 
     public function __construct() {
         if (CACHE_ENABLED) {
-            if (!getenv('REDIS')) {
+            if (!getenv('REDIS_URL')) {
                 $this->redis = new Redis();
                 $this->redis->connect('127.0.0.1', 6379); 
                 $this->local = true;
             } else {
-                $parsed = parse_url(getenv('REDIS'));
+                $parsed = parse_url(getenv('REDIS_URL'));
 
                 $host = $parsed['host'];
                 $port = $parsed['port'];
