6476908bc476aa319884e610dc0660de74cfe6b1
Bob Coret <bob.coret+github@gmail.com>
2025-12-21 18:38:19 +0100

    adding some debugging

--- api/index.php
+++ api/index.php
@@ -12,7 +12,7 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
+    print_r($_ENV);
     exit;
 }
 
